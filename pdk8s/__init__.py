c83b5ffd2d2a5fc9ff3649ba51a869200e5c5793
Florian Ludwig <f.ludwig@greyrook.com>
2020-06-10 18:28:43 +0200

    generate chart

---
+++ pdk8s/__init__.py
@@ -0,0 +1,35 @@
+import yaml
+
+def cleanup(data):
+    if isinstance(data, dict):
+        return cleanup_dict(dict)
+    elif isinstance(data, list):
+        return [cleanup(item) for item in data]
+    else:
+        return data
+
+
+def cleanup_dict(data):
+    """remove all keys with value None
+    """
+    result = {}
+    for key, value in data.items():
+        if value is None:
+            continue
+
+        elif isinstance(value, dict):
+            result[key] = cleanup_dict(value)
+
+        else:
+            result[key] = cleanup(value)
+
+    return result
+
+
+def definition_to_string(k8s_obj) -> str:
+    k8s_def = cleanup_dict(k8s_obj.dict(by_alias=True))
+    return yaml.safe_dump(k8s_def)
+
+
+def chart_to_string(chart) -> str:
+    return "---".join(map(definition_to_string, chart))
