0bb9e27eb00feebf018172ed96602e795ca3cf1d
Florian Ludwig <f.ludwig@greyrook.com>
2020-06-11 01:42:55 +0200

    add update_schema test

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -7,3 +7,30 @@ def test_version_re():
     assert VERSION.match("v1")
     assert VERSION.match("v1alpha")
     assert not VERSION.match("alpha")
+
+
+def test_update_schema():
+    schema = {
+        "definitions": {
+            "io.k8s.api.rbac.v1.ClusterRole": {
+                "properties": {
+                    "apiVersion": {"type": "string"},
+                    "kind": {"type": "string", "enum": ["ClusterRole"]},
+                    "status": {"type": "string"},
+                },
+                "type": "object",
+                "x-kubernetes-group-version-kind": [
+                    {
+                        "group": "rbac.authorization.k8s.io",
+                        "kind": "ClusterRole",
+                        "version": "v1",
+                    }
+                ],
+            }
+        }
+    }
+    pdk8s.gen_k8s.update_schema(schema)
+
+    cluster_role = schema["definitions"]["io.k8s.api.rbac.v1.ClusterRole"]
+    assert cluster_role["properties"]["apiVersion"]["default"] == "v1"
+    assert cluster_role["properties"]["kind"]["default"] == "ClusterRole"
