8423e596b2aab943dc1004d0bbf8f1480e2b6a99
Florian Ludwig <f.ludwig@greyrook.com>
2020-06-11 02:07:23 +0200

    named model, no with getter and setter for name

--- pdk8s/model.py
+++ pdk8s/model.py
@@ -1,15 +1,39 @@
+from typing import Optional
+
 import pydantic
 
 
+def _set_name(metadata, value):
+    if metadata is None:
+        return {"name": value}
+    
+    if isinstance(metadata, dict):
+        metadata["name"] = value
+    else:
+        metadata.name = value
+    
+    return metadata
+
+
 class NamedModel(pydantic.BaseModel):
+    def _get_name(self) -> Optional[str]:
+        if not self.metadata:
+            return None
+        return self.metadata.name
+    
+    name = property(_get_name)
+
+    def __setattr__(self, name, value):
+        if name == "name":
+            self.metadata = _set_name(self.metadata, value)
+        else:
+            super().__setattr__(name, value)
+
     @pydantic.root_validator(pre=True)
     def extract_name(cls, values):
+        print("Validator!!!", values)
         if "name" in values:
-            if "metadata" in values:
-                raise NotImplementedError("provide name or metadata")
-            
-            values["metadata"] = {"name": values["name"]}
+            values["metadata"] = _set_name(values.get("metadata"), values["name"])
             del values["name"]
 
         return values
-
---
+++ tests/test_model.py
@@ -0,0 +1,41 @@
+import pdk8s.model
+import pdk8s.k8s
+
+
+class Awesome(pdk8s.model.NamedModel):
+    class Config:
+        extra = "allow"
+        validate_assignment = True
+    metadata: pdk8s.k8s.ObjectMeta
+
+
+def test_named_model():
+    foo = Awesome(name="foo")
+    assert foo.metadata.name == "foo"
+    assert foo.name == "foo"
+    assert "name" not in foo.dict()
+
+    # change name
+    foo.name = "new_foo"
+    assert foo.metadata.name == "new_foo"
+    assert foo.name == "new_foo"
+
+    # init with metadata dict
+    bar = Awesome(name="bar", metadata={"clusterName": "Yeah"})
+    assert bar.metadata.name == "bar"
+    assert bar.metadata.cluster_name == "Yeah"
+
+    # init with metadata object
+    metadata = pdk8s.k8s.ObjectMeta(cluster_name="hell_yeah")
+    bar = Awesome(name="bar", metadata=metadata)
+    assert bar.metadata.name == "bar"
+    assert bar.metadata.cluster_name == "hell_yeah"
+    assert metadata.name == "bar"
+
+    # init with metadata object with name
+    # we expect the existing name to be overwritten
+    metadata = pdk8s.k8s.ObjectMeta(name="ops", cluster_name="hell_yeah")
+    bar = Awesome(name="bar", metadata=metadata)
+    assert bar.metadata.name == "bar"
+    assert bar.metadata.cluster_name == "hell_yeah"
+    assert metadata.name == "bar"
