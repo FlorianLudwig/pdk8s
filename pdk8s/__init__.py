1b21d0a7ba3691f4782f6231140c29d450633d4e
Florian Ludwig <f.ludwig@greyrook.com>
2020-06-12 12:40:20 +0200

    Chart is just a placeholder for now

--- pdk8s/__init__.py
+++ pdk8s/__init__.py
@@ -2,8 +2,8 @@ import yaml
 
 __version__ = "0.1.1"
 
-class Chart:
-    def __init__(self, context, ns: str):
-        self.context = context
-        self.ns = ns
+
+class Chart(list):
+    def __init__(self):
+        pass
 
